31fdbd69550dd21caf74b1947a4581d44b1a25b6
Kushaagra <mehtakushaagra@gmail.com>
2024-03-31 00:38:54 +0530

    suggestor api done

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ model = genai.GenerativeModel(model_name="gemini-1.0-pro",
                               generation_config=generation_config,
                               safety_settings=safety_settings)
 
-prompts = "I'm looking for a {} trip to {} that fits within my {}. My travel interests include {} and I prefer a {} experience. I'm planning to travel during {} and my current location is {}"
+prompts = " Suggest 5 exciting travel destinations that perfectly match my preferences. I'm looking for a {} days trip to a {} that fits within my budget of {}. My travel interests include {} and I prefer a {} experience. I'm planning to travel during {} and my current location is {}. Answer is the following format **suggestion 1** - description about that place and so on for 5 suggestions "
 
 
 
@@ -42,11 +42,15 @@ app = Flask(__name__)
 def home():
     return "working"
 
-
 @app.route('/home', methods=['GET','POST'])
 def homepage():
+    return render_template("Homepage.html")
+
+
+@app.route('/suggestor', methods=['GET','POST'])
+def suggestor():
     if request.method == "GET":
-        return render_template("Homepage.html")
+        return render_template("Suggestor.html")
     elif request.method == "POST":
         data = request.form
         duration = data['duration']
@@ -61,7 +65,20 @@ def homepage():
         jsonresp = ["input:{}".format(final_prompt),
                     "output: ",
         ]
-        return jsonresp
+
+        response = model.generate_content(jsonresp)
+        newresp = response.text.replace("** * **", "<strong>")
+        newresp = newresp.replace(":**", "</strong>")
+        newresp = newresp.replace(". **", "<br>")
+        newresp = newresp.replace("**", "")
+        newresp = newresp.replace("*", "<br>")
+        newresp = newresp.replace("Suggestion", "<br><br>Suggestion")
+
+
+
+        return newresp, 200
+
+        # return jsonresp
 
 
 if __name__ == '__main__':
